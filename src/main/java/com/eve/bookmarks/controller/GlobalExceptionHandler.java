0d6eff345b4b8c701b85e4604f01aa3cb0bbf4d9
cyjiang <13721074873@163.com>
2018-11-10 17:01:02 +0800

    1去除todo

--- src/main/java/com/eve/bookmarks/controller/GlobalExceptionHandler.java
+++ src/main/java/com/eve/bookmarks/controller/GlobalExceptionHandler.java
@@ -11,9 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 @Component
 public class GlobalExceptionHandler implements HandlerExceptionResolver {
 
-    /**
-     * TODO 简单描述该方法的实现功能（可选）.
-     */
+
     public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler,
                                          Exception ex) {
         System.out.println("This is exception handler method!");
